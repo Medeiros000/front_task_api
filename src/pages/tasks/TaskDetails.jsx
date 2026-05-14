ad059b03d2e2d0780d3990e90906999e2dbf9140
Júnior Medeiros <98979459+Medeiros000@users.noreply.github.com>
2026-05-14 11:44:05 -0300

    Mobile UI update

--- src/pages/tasks/TaskDetails.jsx
+++ src/pages/tasks/TaskDetails.jsx
@@ -8,58 +8,64 @@ function TaskDetails() {
 	const [msg, setMsg] = useState("");
 	const taskId = useParams().id;
 	const { refetch } = useOutletContext() || {};
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	const [carregador, setCarregador] = useState(true);
+
+	function Carregador() {
+		return (
+			<svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+				<circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
+			</svg>
+		);
+	}
 
 	useEffect(() => {
 		taskService.getById(taskId).then((task) => {
 			setTask(task);
+      setCarregador(false);
 		});
 	}, [taskId]);
 
-  useEffect(() => {
-    if (msg) {
-      const timer = setTimeout(() => {
-        setMsg("");
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, [msg]);
+	useEffect(() => {
+		if (msg) {
+			const timer = setTimeout(() => {
+				setMsg("");
+			}, 3000);
+			return () => clearTimeout(timer);
+		}
+	}, [msg]);
 
 	function handleDeleteTask() {
-		taskService.delete(taskId).then(() => {
-		  setTask(null);
-      if (refetch) refetch();
-      navigate(`/tasks`);
-		}).catch((error) => {
-      if (error.status === 403) {
-        setMsg("You can only delete your own tasks");
-      }
-      setMsg("You can only delete your own tasks");
-    });
+		taskService
+			.delete(taskId)
+			.then(() => {
+				setTask(null);
+				if (refetch) refetch();
+				navigate(`/tasks`);
+			})
+			.catch((error) => {
+				if (error.status === 403) {
+					setMsg("You can only delete your own tasks");
+				}
+				setMsg("You can only delete your own tasks");
+			});
 	}
 
 	if (!task) {
-		navigate(`/tasks`);
-		return (
-			<div>
-				<p>Task not found</p>
-			</div>
-		);
+	  return <Carregador />;
 	}
 
 	return (
-		<div className="task-details-container">
+		<div className="task-details-container">				
 			<h2>Task Details</h2>
 			<div className="task-details-inner center-item">
 				<div>
 					<Link to={"info"}>Info</Link>
 					<Link to={"update"}>Update</Link>
-					<Link onClick={handleDeleteTask}>
-						Delete
-					</Link>
+					<Link onClick={handleDeleteTask}>Delete</Link>
 				</div>
 				<Outlet context={{ task, refetch }} />
-        {msg && <p className="error">{msg}</p>}
+				{msg && <p className="error">{msg}</p>}
 			</div>
 		</div>
 	);
